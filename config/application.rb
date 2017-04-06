cd9c7fd4cca2a1ff17816e7fe45fb6dae686b4fb
namazu <namazu510@hotmail.co.jp>
2017-04-06 15:40:57 +0900

    Fix 表示時間をtokyoに変更

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module TeuSchoolBusApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # 時間をtokyoに
+    config.time_zone = 'Tokyo'
   end
 end
